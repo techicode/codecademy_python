c05fcd1fc2de6400a87b6481e667c3ff0fbe0660
Luis <techicode@outlook.com>
2021-10-31 20:19:37 -0300

    first projects

---
+++ hello_world/basic_syntax.py
@@ -0,0 +1,22 @@
+# this a comment
+
+# print("Hello, World!")
+print("Hello, World!")
+
+# variable  (must be name with lowercase and underscore)
+day_week = "Monday"
+
+# concatenation of string variable with number variable
+age = 10
+i_am_msg = "My age is: "
+
+# age must be cast to string using str()
+full_msg = i_am_msg + str(age)
+print(full_msg)  # My age is: 10
+
+# declare multiple line string
+example_string = """
+This is a triple
+line string 
+so here is the last line
+"""
---
+++ hello_world/control_flow.py
@@ -0,0 +1,13 @@
+# define boolean variables
+this_true = True
+this_false = False
+# remember to make first letter of the boolean capital
+
+# relational operators
+# == equal to
+# != not equal to
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater than or equal to
+
