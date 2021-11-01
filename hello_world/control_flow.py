29a271154a8cc1be46c57de15d3b3f2fdf9c8189
techicode <techicode@outlook.com>
2021-10-31 23:43:30 -0300

    control flow done

--- hello_world/control_flow.py
+++ hello_world/control_flow.py
@@ -11,3 +11,25 @@ this_false = False
 # <= less than or equal to
 # >= greater than or equal to
 
+# if conditional
+if 2 == 2:
+    print("2 is equal to 2")
+
+# boolean operator AND OR NOT
+
+grade = "A"
+assistance_percentage = 90
+
+# and
+if grade == "A" and assistance_percentage >= 90:
+    print("You are passing")
+
+# or
+if grade == "B" or assistance_percentage >= 90:
+    print("You are passing")
+
+# else conditional
+if grade == "A":
+    print("You are passing with an A!!")
+else:
+    print("You are failing, so sad")
