3662ed530d71029e7398292490d4a193139e90f7
techicode <techicode@outlook.com>
2021-11-01 00:24:21 -0300

    starting with lists

---
+++ hello_world/lists.py
@@ -0,0 +1,13 @@
+# declare a list
+list_number = [1, 2, 3, 4, 5]
+
+# list methods
+
+# append
+garden = []
+garden.append('tomato')
+
+# another way is just using plus concatenation
+garden += ['carrot', 'potato']
+print(garden)
+
