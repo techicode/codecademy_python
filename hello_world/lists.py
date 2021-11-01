463ac3b32310325235b9d9e98ded6415ee53e510
Luis <techicode@outlook.com>
2021-11-01 07:02:27 -0300

    done with first part of lists

--- hello_world/lists.py
+++ hello_world/lists.py
@@ -9,5 +9,24 @@ garden.append('tomato')
 
 # another way is just using plus concatenation
 garden += ['carrot', 'potato']
-print(garden)
+print(garden)  # ['tomato', 'carrot', 'potato']
 
+# modify a element of a list
+garden[0] = 'apple'
+print(garden)  # ['apple', 'carrot', 'potato']
+
+# remove a element of a list
+garden.remove('carrot')
+print(garden)  # ['apple', 'potato']
+
+# multidimensional list
+garden = [['tomato', 'carrot'], ['potato', 'cucumber']]
+print(garden)  # [['tomato', 'carrot'], ['potato', 'cucumber']]
+
+# replace element from multi-dimensional list
+garden[0][0] = 'apple'
+print(garden)  # [['apple', 'carrot'], ['potato', 'cucumber']]
+
+# append element to multi-dimensional list
+garden[0].append('pear')
+print(garden)  # [['apple', 'carrot', 'pear'], ['potato', 'cucumber']]
